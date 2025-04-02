22ad5a8b4a78d0f41e945b92b56b979c72f4765c
behnam farhadi <farhadi.behnam@gmail.com>
2025-04-02 16:44:11 -0230

    pass only current step with using if else-step4

--- scripts/validation.js
+++ scripts/validation.js
@@ -60,7 +60,36 @@ document.addEventListener('DOMContentLoaded', function () {
                 second_category: document.getElementById('second-category').value,
                 virtual_tour: document.getElementById('virtual-tour').value
             };
+        } else if (currentStep === 4) {
+            formData = {
+                size: document.getElementById('size').value,
+                capacity: document.getElementById('capacity').value,
+                bathroom: document.getElementById('bathroom').value,
+                individual_worker: document.getElementById('individual-worker').value,
+                client_meeting: document.getElementById('client-meeting').value,
+                good_for_photoshoot: document.getElementById('good-for-photoshoot').value,
+                team_meeting: document.getElementById('team-meeting').value,
+                details_list: document.getElementById('details-list').value,
+                terms: document.getElementById('terms').value,
+                rules: document.getElementById('rules').value,
+            };
         }
+        // } else if (currentStep === 2) {
+        //     formData = {
+        //         booking_type: document.getElementById('booking-type').value,
+        //         price_per_hour: document.getElementById('price-per-hour').value,
+        //         before_label: document.getElementById('before-label').value,
+        //         after_label: document.getElementById('after-label').value,
+        //         taxes: document.getElementById('taxes').value,
+        //         cleaning_fee: document.getElementById('cleaning-fee').value,
+        //         minimum_hours: document.getElementById('minimum-hours').value,
+        //         security_deposit: document.getElementById('security-deposit').value,
+        //         early_bird_discount: document.getElementById('early-bird-discount').value,
+        //         start_hour: document.getElementById('start-hour').value,
+        //         end_hour: document.getElementById('end-hour').value,
+        //         price_calendar: document.getElementById('price-calendar').value
+        //     };
+        // }
 
         // Validate inputs before saving and proceeding
         const inputs = document.querySelectorAll(`.step-${currentStep} input[required], .step-${currentStep} textarea[required], .step-${currentStep} select[required]`);
