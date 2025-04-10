52604bfd32e8aed61b43f0d758dd8aac22f71b78
behnam farhadi <farhadi.behnam@gmail.com>
2025-04-10 10:01:34 -0230

    Done with add listing Js

--- scripts/add-listing.js
+++ scripts/add-listing.js
@@ -61,16 +61,14 @@ PreviousBtn.addEventListener("click", () => {
   updateFormVisibility(); // Update form and progress bar
 });
 
-// finishBtn.addEventListener("click", () => {
-  function sConsole(event) {
-  event.preventDefault();
-  var data = document.getElementById("title, category, room_type, guest_no, city, neighborhood, property_description, booking_type, price_per_hour, before_label, after_label, taxes, cleaning_fee, minimum_hours, security_deposit, early_bird_discount");
-  console.log(title.value);
-  
-// }
-  // location.reload(); // Reload the page to reset the form
-  }
+function submitForm() {
+  formData = {
+    ...formData,
+    availabilityDates: window.availabilityDates || [],
+  };
 
+  console.log(formData);
+}
 
 const { Calendar } = window.VanillaCalendarPro;
 
@@ -81,11 +79,10 @@ const options = {
   selectedTheme: "light",
   onClickDate(self) {
     const { selectedDates } = self.context;
-    console.log(selectedDates);
+    window.availabilityDates = selectedDates;
   },
 };
 
 const calendar = new Calendar("#calendar", options);
 
 calendar.init();
-
